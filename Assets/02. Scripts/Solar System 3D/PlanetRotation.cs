6e479001ab6c12327c3c066c3711c3ef8db86a21
joybox-js <ai.joybox@gmail.com>
2025-05-26 17:32:30 +0900

    2025-05-26

--- Assets/02. Scripts/Solar System 3D/PlanetRotation.cs
+++ Assets/02. Scripts/Solar System 3D/PlanetRotation.cs
@@ -12,10 +12,10 @@ public class PlanetRotation : MonoBehaviour
 
     void Update()
     {
-        // 자기 자신이 회전하는 기능
-        transform.Rotate(Vector3.up * (rotSpeed * Time.deltaTime));
+        //  자전하는 회전하는 기능
+        transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime, Space.Self);
 
-        if (isRevolution == true)
+        if (isRevolution == true) // 조건문 -> 만약 공전을 한다면,
         {
             // 공전하는 기능
             transform.RotateAround(targetPlanet.position, Vector3.up, revolutionSpeed * Time.deltaTime);
