1ecab35168a7b62756d8f423542a9dec8c20ce4a
joybox-js <ai.joybox@gmail.com>
2025-05-26 10:00:09 +0900

    2025-05-23(2/2)
    행성

--- Assets/02. Scripts/Movement.cs
+++ Assets/02. Scripts/Movement.cs
@@ -1,22 +1,19 @@
 using UnityEngine;
  
- public class Movement : MonoBehaviour
- {
-     public float moveSpeed = 5f;
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
      
-     void Update()
-     {
-         // 부드럽게 증감하는 값
-         float h = Input.GetAxis("Horizontal");
-         float v = Input.GetAxis("Vertical");
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Vector3 normalDir = dir.normalized;
          
-         // // 딱 떨어지는 값
-         // float h = Input.GetAxisRaw("Horizontal");
-         // float v = Input.GetAxisRaw("Vertical");
-         
-         Vector3 dir = new Vector3(h, 0, v);
-         Debug.Log($"현재 입력 : {dir}");
-         
-         transform.position += dir * moveSpeed * Time.deltaTime;
-     }
- }
+        transform.position += moveSpeed * Time.deltaTime * normalDir;
+
+        transform.LookAt(transform.position + normalDir);
+    }
+}
